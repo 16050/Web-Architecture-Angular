7de38967717ab84397a5c569f7ef5cb55ea5395f
El bakkoury Yassine <16050@ecam.be>
2019-12-11 12:13:58 +0100

    Start Angular

---
+++ GearSportAngular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SportComponent } from './sport/sport.component';
+import { CategoryComponent } from './category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SportComponent,
+    CategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
