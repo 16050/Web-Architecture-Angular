6e1f9aa7e774f41097120597e0125831bd8b065c
El bakkoury Yassine <16050@ecam.be>
2019-12-26 20:58:14 +0100

    Add componnents
    gear, sport, category and navbar

--- GearSportAngular/src/app/app.module.ts
+++ GearSportAngular/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SportComponent } from './sport/sport.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GearComponent } from './gear/gear.component';
 import { CategoryComponent } from './category/category.component';
+import { SportComponent } from './sport/sport.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    GearComponent,
+    CategoryComponent,
     SportComponent,
-    CategoryComponent
   ],
   imports: [
     BrowserModule,
